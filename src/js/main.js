81fdad77e51857c403df611bfc6426c63a4a6a97
Owanate <owanate999@gmail.com>
2024-01-09 09:10:41 +0100

    Add error handling using the try catch block and the .catch promise chaining

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,6 @@ document.querySelector(".nav-toggler").addEventListener("click", () => {
   navbar.classList.toggle("navbar--visible");
 });
 
-// Get APIKey
-const apiKey = "f930a8b2e71bc550aac09c0222dd9f2d";
-const forecastApiKey = "bb54be0147ot8a4cca9066da1f16f233";
-
 // Set the Current Date
 const date = document.querySelector("#date");
 const time = document.querySelector("#time");
@@ -28,64 +24,55 @@ function convertTimestamp(timestamp) {
 
 // Display the Weather data
 function displayWeather(lat, lon) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  fetch(url)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f930a8b2e71bc550aac09c0222dd9f2d&units=metric`)
     .then((response) => response.json())
     .then((data) => {
-      document.querySelector(
-        ".weather-icon"
-      ).src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      document.querySelector(".weather-icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       document.querySelector(".weather-icon").alt = data.weather[0].description;
       document.querySelector("#temperature").textContent = data.main.temp;
-      document.querySelector("#description").textContent =
-        data.weather[0].description;
-      document.querySelector(
-        "#location"
-      ).textContent = `${data.name}, ${data.sys.country}`;
+      document.querySelector("#description").textContent = data.weather[0].description;
+      document.querySelector("#location").textContent = `${data.name}, ${data.sys.country}`;
       document.querySelector("#clouds").textContent = `${data.clouds.all}%`;
       document.querySelector("#max-temp").textContent = `${data.main.temp_max}°C`;
       document.querySelector("#min-temp").textContent = `${data.main.temp_min}°C`;
-      document.querySelector(
-        "#feels-like"
-      ).textContent = `${data.main.feels_like}°C`;
+      document.querySelector("#feels-like").textContent = `${data.main.feels_like}°C`;
       document.querySelector("#wind").textContent = `${data.wind.speed} km/h`;
-      document.querySelector("#visibility").textContent = `${
-        data.visibility / 1000
-      }.0 km`;
-      document.querySelector(
-        "#pressure"
-      ).textContent = `${data.main.pressure} mb`;
+      document.querySelector("#visibility").textContent = `${data.visibility / 1000}.0 km`;
+      document.querySelector("#pressure").textContent = `${data.main.pressure} mb`;
       document.querySelector("#humidity").textContent = `${data.main.humidity} %`;
       const sunrise = convertTimestamp(data.sys.sunrise);
-      document.querySelector(
-        "#sunrise"
-      ).textContent = `${sunrise.hours}:${sunrise.minutes} am`;
+      document.querySelector("#sunrise").textContent = `${sunrise.hours}:${sunrise.minutes} am`;
       const sunset = convertTimestamp(data.sys.sunset);
-      document.querySelector(
-        "#sunset"
-      ).textContent = `${sunset.hours}:${sunset.minutes} pm`;
-    });
+      document.querySelector("#sunset").textContent = `${sunset.hours}:${sunset.minutes} pm`;
+    })
+    .catch(console.error("Fetch", error));
 }
 
 // Weather Forecast
 async function forecast(lat, lon) {
-  const url = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${forecastApiKey}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  const result = data.daily.slice(1, 5);
-  result.map((value) => {
-    const tomorrow = convertTimestamp(value.time);
-    const forecast = `<div class="forecast-col">
-          <img src="${value.condition.icon_url}" alt="${value.condition.icon}">
-          <p class="forecast-temp">${value.temperature.day.toFixed(1)}°C</p>
-          <p class="forecast-day" id="forecast-date">${tomorrow.date} ${
-      tomorrow.month
-    }</p>
-        </div>`;
-    document
-      .querySelector(".forecast-row")
-      .insertAdjacentHTML("beforeend", forecast);
-  });
+  try {
+    const response = await fetch(`https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=bb54be0147ot8a4cca9066da1f16f233`);
+    if (response.ok){
+      const data = await response.json();
+      const result = data.daily.slice(1, 5);
+      result.map((value) => {
+        const tomorrow = convertTimestamp(value.time);
+        const forecast = 
+          `<div class="forecast-col">
+            <img src="${value.condition.icon_url}" alt="${value.condition.icon}">
+            <p class="forecast-temp">${value.temperature.day.toFixed(1)}°C</p>
+            <p class="forecast-day" id="forecast-date">${tomorrow.date} ${tomorrow.month}</p>
+          </div>`;
+        document.querySelector(".forecast-row").insertAdjacentHTML("beforeend", forecast);
+      });
+    } else {
+      if (response.status === 404) throw new Error("404, Not found");
+      if (response.status === 500) throw new Error("500, internal server error");
+      throw new Error(response.status);
+    }
+  } catch (error){
+    console.error("Fetch", error);
+  }
 }
 
 // Weather of current position
@@ -117,12 +104,12 @@ document.addEventListener("DOMContentLoaded", () => {
 // Search Form
 let searchBtn = document.querySelector("#search-btn");
 searchBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    const searchQuery = document.querySelector("#search-input").value;
-    if (!searchQuery) {
-      alert('Must provide a city to search...');
-      throw new Error('No search query found');
-    }
-    localStorage.setItem("searchQuery", searchQuery);
-    window.location.href = './src/search.html';
+  event.preventDefault();
+  const searchQuery = document.querySelector("#search-input").value;
+  if (!searchQuery) {
+    alert('Must provide a city to search...');
+    throw new Error('No search query found');
+  }
+  localStorage.setItem("searchQuery", searchQuery);
+  window.location.href = './src/search.html';
 });
--- src/js/search.js
+++ src/js/search.js
@@ -1,10 +1,5 @@
 import { displayDateTime } from "./date.js";
 
-let query = localStorage.getItem("searchQuery");
-
-// Get APIKey
-const apiKey = "f930a8b2e71bc550aac09c0222dd9f2d";
-
 // Set the Current Date
 const date = document.querySelector("#date");
 const time = document.querySelector("#time");
@@ -12,27 +7,23 @@ displayDateTime(date, time);
 
 // Search Result
 document.addEventListener("DOMContentLoaded", () => {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}`)
+  let query = localStorage.getItem("searchQuery");
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=$f930a8b2e71bc550aac09c0222dd9f2d`)
     .then((response) => response.json())
     .then((data) => {
-      document.querySelector(
-        ".location"
-      ).textContent = `${data.name}, ${data.sys.country}`;
+      document.querySelector(".location").textContent = `${data.name}, ${data.sys.country}`;
       document.querySelector(".temperature").textContent = `${data.main.temp}°C`;
       document.querySelector(".main").textContent = `${data.weather[0].main}`;
-      document.querySelector(
-        ".description"
-      ).textContent = `${data.weather[0].description}`;
+      document.querySelector(".description").textContent = `${data.weather[0].description}`;
       document.querySelector("#wind").textContent = `${data.wind.speed} km/h`;
-      document.querySelector(
-        "#pressure"
-      ).textContent = `${data.main.pressure} mb`;
+      document.querySelector("#pressure").textContent = `${data.main.pressure} mb`;
       document.querySelector("#humidity").textContent = `${data.main.humidity} %`;
       let value = query.split(" ").join("-");
-      let image = `url(https://source.unsplash.com/1600x900/?${value}) center center/cover no-repeat`
-        if(image) {
-            document.body.style.background = image;
-            document.body.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
-        }
-    });
+      let image = `url(https://source.unsplash.com/1600x900/?${value}) center center/cover no-repeat`;
+      if (image) {
+        document.body.style.background = image;
+        document.body.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
+      }
+    })
+    .catch(console.error("Fetch", error));
 });
